212b8226b944d7d6908123e4cb69fa25d890f104
Muhamad Kamil <kamilmatnoor@gmail.com>
2020-05-13 12:50:06 +0800

    fixed issue input field always refresh the whole page upon ENTER pressed

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular';
+
+  constructor() {
+    $(function () {
+      $("form").submit(function () { return false; });
+    })
+  }
 }
